7ed139c47f829c6ef820b156129e68275ac6c4c8
Farhan Franaka <farhanfranaka0505@gmail.com>
2024-11-25 19:11:53 +0700

    add user api

---
+++ bun-hono-restful-api/src/controller/user-controller.ts
@@ -0,0 +1,68 @@
+import { Hono } from "hono";
+import {
+  LoginUserRequest,
+  RegisterUserRequest,
+  toUserResponse,
+  UpdateUserRequest,
+} from "../model/user-model";
+import { UserService } from "../service/user-service";
+import { ApplicationVariables } from "../model/app-model";
+import { User } from "@prisma/client";
+
+export const userController = new Hono<{
+  Variables: ApplicationVariables;
+}>();
+
+userController.post("/api/user", async (c) => {
+  const request = (await c.req.json()) as RegisterUserRequest;
+  const response = await UserService.register(request);
+
+  return c.json({
+    data: response,
+  });
+});
+
+userController.post("/api/user/login", async (c) => {
+  const request = (await c.req.json()) as LoginUserRequest;
+  const response = await UserService.login(request);
+
+  return c.json({
+    data: response,
+  });
+});
+
+userController.use(async (c, next) => {
+  const token = c.req.header("Authorization");
+  const user = await UserService.get(token);
+
+  c.set("user", user);
+
+  await next();
+});
+
+userController.get("/api/user/current", async (c) => {
+  const user = c.get("user") as User;
+
+  return c.json({
+    data: toUserResponse(user),
+  });
+});
+
+userController.patch("/api/user/current", async (c) => {
+  const user = c.get("user") as User;
+  const request = (await c.req.json()) as UpdateUserRequest;
+  const response = await UserService.update(user, request);
+
+  return c.json({
+    data: response,
+  });
+});
+
+userController.delete("/api/user/current", async (c) => {
+  const user = c.get("user") as User;
+  const response = await UserService.logout(user);
+
+  return c.json({
+    data: response,
+  });
+});
--- bun-hono-restful-api/src/index.ts
+++ bun-hono-restful-api/src/index.ts
@@ -1,9 +1,32 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { userController } from "./controller/user-controller";
+import { HTTPException } from "hono/http-exception";
+import { ZodError } from "zod";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default app
+app.route("/", userController);
+
+app.onError(async (error, c) => {
+  if (error instanceof HTTPException) {
+    return c.json({
+      errors: error.message,
+    });
+  } else if (error instanceof ZodError) {
+    c.status(400);
+    return c.json({
+      errors: error.message,
+    });
+  } else {
+    c.status(500);
+    return c.json({
+      errors: error.message,
+    });
+  }
+});
+
+export default app;
